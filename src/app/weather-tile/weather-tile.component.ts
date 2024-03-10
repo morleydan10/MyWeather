983f597163a53d230f9f2467370e13465b01c9de
Daniel Morley <morleydan10@gmail.com>
2024-03-10 18:14:29 -0400

    addded header with navbar to app

---
+++ src/app/weather-tile/weather-tile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-tile',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-tile.component.html',
+  styleUrl: './weather-tile.component.css'
+})
+export class WeatherTileComponent {
+
+}
